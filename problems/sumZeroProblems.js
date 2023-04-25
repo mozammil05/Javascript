7de3842b006dcf3424ef9a80994f26c232a57222
mozammil <mozammil.raja@antiersolutions.com>
2023-04-25 13:12:12 +0530

    sum zeros problems

---
+++ problems/sumZeroProblems.js
@@ -0,0 +1,13 @@
+const arr = [-5,-4,-3,-2,0,2,4,6,8];
+
+function sumZero(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
+
+console.log(sumZero(arr)); // output: [-2, 2]
